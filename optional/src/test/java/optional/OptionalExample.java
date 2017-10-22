fba57a4c81c0e5477c31c0707c21c0c6f97b1669
Viacheslav <slavastap@gmail.com>
2017-10-22 10:28:40 +0300

    Optional Exercise : Optional Example is done
    - Added javaDoc to getOptional method
    - flatMap and map tests are implemented
    - test of orElseThrow behavior moved to separate test method

--- optional/src/test/java/optional/OptionalExample.java
+++ optional/src/test/java/optional/OptionalExample.java
@@ -2,9 +2,12 @@ package optional;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,12 +16,19 @@ public class OptionalExample {
 
     @Test
     public void get() {
+        // Get empty optional. IsPresent always return false
         Optional<String> o1 = Optional.empty();
 
         o1.ifPresent(s -> System.out.println(s));
 
-        o1.orElse("t");
-        o1.orElseGet(() -> "t");
+        assertEquals("t", o1.orElse("t"));
+        // if not isPresent - return function result
+        assertEquals("t", o1.orElseGet(() -> "t"));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldThrowException() {
+        Optional<String> o1 = Optional.empty();
         o1.orElseThrow(() -> new UnsupportedOperationException());
     }
 
@@ -53,14 +63,49 @@ public class OptionalExample {
 
     @Test
     public void flatMap() {
-        throw new UnsupportedOperationException("Not implemented");
+        Optional<String> optional = getOptional();
+
+        // Create a flat mapper which add symbols from source string to optional
+        Function<String, Optional<List<Character>>> flatMapper = str -> {
+            Optional<List<Character>> chars = Optional.of(new ArrayList<Character>());
+            str.chars().forEach(symbol -> chars.get().add((char) symbol));
+            return chars;
+        };
+
+        // Apply function if value is present
+        Optional<List<Character>> actual = optional.flatMap(flatMapper);
+
+        // Check expected behavior
+        Optional<List<Character>> expected;
+        if (optional.isPresent()) {
+            expected = flatMapper.apply(optional.get());
+        } else {
+            expected = Optional.empty();
+        }
+        assertEquals(expected, actual);
     }
 
     @Test
     public void filter() {
-        throw new UnsupportedOperationException("Not implemented");
+        Optional<String> optional = getOptional();
+        Predicate<String> predicate = str -> str.contains("a");
+        // Optional filter will check value by predicate if this value is present
+        Optional<String> actual = optional.filter(predicate);
+        // Check filter behavior
+        Optional<String> expected;
+        if (optional.isPresent()) {
+            expected = predicate.test(optional.get()) ? optional : Optional.empty();
+        } else {
+            expected = Optional.empty();
+        }
+        assertEquals(expected, actual);
     }
 
+    /**
+     * Create pseudo-random Optional
+     *
+     * @return Pseude-random optional
+     */
     private Optional<String> getOptional() {
         return ThreadLocalRandom.current().nextBoolean() ? Optional.empty() : Optional.of("abc");
     }
